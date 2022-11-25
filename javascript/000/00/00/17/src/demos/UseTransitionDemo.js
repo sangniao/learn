50383497dd47fef457696ab43c8661b21994b097
sangniao <sangniao@pctco.com>
2022-11-25 14:37:02 +0800

    javascript - 000/00/00/17

---
+++ javascript/000/00/00/17/src/demos/UseTransitionDemo.js
@@ -0,0 +1,59 @@
+import React, { useState, useCallback } from "react";
+import { useTransition, animated } from "react-spring";
+import "../style.css";
+
+const items = [
+  ({ style }) => (
+    <animated.div
+      style={{
+        ...style,
+        background:
+          "url(https://images.pexels.com/photos/1904769/pexels-photo-1904769.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500) no-repeat"
+      }}
+    />
+  ),
+  ({ style }) => (
+    <animated.div
+      style={{
+        ...style,
+        background:
+          "url(https://images.pexels.com/photos/775201/pexels-photo-775201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500) no-repeat"
+      }}
+    />
+  ),
+  ({ style }) => (
+    <animated.div
+      style={{
+        ...style,
+        background:
+          "url(https://images.pexels.com/photos/2387866/pexels-photo-2387866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500) no-repeat"
+      }}
+    />
+  )
+];
+
+export const UseTransitionDemoPage = () => {
+  const [index, setIndex] = useState(0);
+  const onClick = useCallback(
+    () => setIndex(state => (state + 1) % items.length),
+    []
+  );
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 0, transform: "translate3d(0, 100%, 0)" },
+    enter: { opacity: 1, transform: "translate3d(0, 0%, 0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
+  });
+
+  console.log(transitions);
+
+  return (
+    <div className="useTransitionDemoPage">
+      <div className="simpleTransitionDemo" onClick={onClick}>
+        {transitions.map(({ item, props, key }) => {
+          const Page = items[item];
+          return <Page key={key} style={props} />;
+        })}
+      </div>
+    </div>
+  );
+};
